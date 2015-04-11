8fb8f834d96cf4a5fe3e896855e58bdf33f16ec6
Jason Zhang <zyj163loveher@163.com>
2015-04-11 16:15:13 +0800

    revert
    This reverts commit fca89c150ce1fabf41955db79b93d7cd7cd4b65e.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default_url_options = {host: Settings.site.domain}
+  config.action_mailer.default_url_options = {host: Settings.site.domain}
   config.action_mailer.smtp_settings = Settings.site_mailer.to_h
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,4 @@
 Bugsnag.configure do |config|
-  # config.api_key = Settings.bugsnag.api_key
+  config.api_key = Settings.bugsnag.api_key
   config.notify_release_stages = ["production"]
 end
