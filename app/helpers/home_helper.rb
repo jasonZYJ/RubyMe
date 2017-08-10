d8416294662a14bbf05366bcd67345248d296089
JasonZYJ <zyj880218@gmail.com>
2017-08-10 21:53:31 +0800

    refactor home helper:remove similar code

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,9 +1,13 @@
 module HomeHelper
   def category_count_hash category_ids
-    Post.unscoped.joins(:category).select("categories.id as category_id, count(posts.id)").where(categories: { id: category_ids }).group("categories.id").index_by(&:category_id)
+    count_hash(:category, "categories.id as category_id, count(posts.id)", { categories: { id: category_ids } }, "categories.id", :category_id)
   end
 
-  def post_count_hash post_ids  #TODO frontend
-    Post.unscoped.joins(:replies).select("posts.id as post_id, count(replies.id)").where(posts: { id: post_ids}).group("posts.id").index_by(&:post_id)
+  def post_count_hash post_ids
+    count_hash(:replies, "posts.id as post_id, count(replies.id)", { posts: { id: post_ids } }, "posts.id", :post_id)
+  end
+
+  def count_hash table, select_cols, condition, group_col, index_col
+    Post.unscoped.joins(table).select(select_cols).where(condition).group(group_col).index_by(&index_col)
   end
 end
